142d75966e74ac889b44899859bcb7af287a16fd
Eric Chi <jsdev.chi@gmail.com>
2019-09-02 21:38:32 -0400

    add custom styling for Google Sign In button with conditional classname

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -39,12 +39,14 @@ class SignIn extends React.Component {
         <form onSubmit={this.handleSubmit}>
           <FormInput name='email' type='email' value={this.state.email} handleChange={this.handleChange} label='Email' required />
           <FormInput name='password' type='password' value={this.state.password} handleChange={this.handleChange} label='Password' required />
-          <CustomButton type='submit' value='Submit Form' >
-            Sign In
-          </CustomButton>
-          <CustomButton onClick={signInWithGoogle} >
-            Sign In With Google
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit' value='Submit Form' >
+              Sign In
+            </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn >
+              Sign In With Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     )
