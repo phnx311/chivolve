99f0da78da2654ceebea1f676da519f404cdb643
Eric Chi <ericchi.311@gmail.com>
2019-10-05 14:51:06 -0400

    add styling to collection-item components so that a 'add to cart' button appears on hover

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './collection-item.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
 
 const CollectionItem = ( {id, name, price, imageUrl} ) => (
   <div className='collection-item'>
@@ -11,6 +12,9 @@ const CollectionItem = ( {id, name, price, imageUrl} ) => (
     <div className='collection-footer'>
       <span className='name'>{name}</span>
       <span className='price'>{price}</span>
+    </div>
+    <div className='collection-item-button'>
+      <CustomButton inverted>Add To Cart</CustomButton>
     </div>
   </div>
 )
