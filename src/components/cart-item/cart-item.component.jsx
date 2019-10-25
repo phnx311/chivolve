3b40573e24879449155b769f588ea11e8aa424ab
Eric Chi <ericchi.311@gmail.com>
2019-10-25 10:07:14 -0400

    create dropdown images and item styling to allow for adding of cart items

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import './cart-item/cart-item.styles.scss';
-import { url } from 'inspector';
+import './cart-item.styles.scss';
 
-const CartItem = ({imageUrl}) => (
+const CartItem = ({ item: {imageUrl, name, id, quantity}}) => (
   <div className='cart-item'>
     <div className='cart-image' style={{backgroundImage: `url(${imageUrl})`}}></div>
     <div className='cart-item-info'>
-      <div className='cart-item-name'></div>
-      <div className='cart-item-count'></div>
+      <div className='cart-item-name'>{name}</div>
+      <div className='cart-item-count'>x {quantity}</div>
     </div>
   </div>
 )
--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -4,13 +4,23 @@ import { connect } from 'react-redux';
 
 import './cart.styles.scss';
 
+import CartItem from '../cart-item/cart-item.component';
 import CustomButton from '../custom-button/custom-button.component';
 
-const Cart = () => (
-  <div className='cart'>
-    <div className='cart-items'></div>
-    <CustomButton>Go To Checkout</CustomButton>
-  </div>
-)
+const Cart = ({ cart }) => {
+  console.log(cart, 'what are we iterating over???' )
+  return (
+    <div className='cart'>
+      <div className='cart-items'>
+        {cart.map((cartItem, idx) => (<CartItem item={cartItem} key={idx}/>))}
+      </div>
+      <CustomButton>Go To Checkout</CustomButton>
+    </div>
+  )
+}
 
-export default connect()(Cart);
+const mapStateToProps = (state) => ({
+  cart: state.cart.cartItems
+})
+
+export default connect(mapStateToProps, null)(Cart);
