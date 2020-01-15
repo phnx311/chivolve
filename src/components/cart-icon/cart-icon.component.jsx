eff64710fb5a07885e4c3550b817eb9fceeea65c
Eric Chi <ericchi.311@gmail.com>
2020-01-15 16:30:39 -0500

    creates a cartItemsCount selector that will be used by the cartIcon component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,25 @@
 import React from 'react';
 import { connect } from 'react-redux';
-
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
-
 import { toggleCartDropdown } from '../../redux/cart/cart.action.js';
 import './cart-icon.styles.scss';
 
-const CartIcon = ({ toggleCartDropdown }) => (
+const CartIcon = ({ toggleCartDropdown, cartItemsCount }) => (
   <div className='cart-icon' onClick={toggleCartDropdown}>
     < ShoppingIcon className='shopping-icon'/>
-    <span className='item-count'>0</span>
+<span className='item-count'>{cartItemsCount}</span>
   </div>
 )
+
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartDropdown: () => dispatch(toggleCartDropdown())
 })
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({cart: { cartItems }}) => ({
+  cartItemsCount: cartItems.reduce((acc, cartItem) => {
+    return acc + cartItem.quantity
+  }, 0)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
