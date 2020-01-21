3eb376f90f0ee38c86fdee455d82103310e66fa7
Eric Chi <ericchi.311@gmail.com>
2020-01-21 15:22:40 -0500

    add style for checkout header and add a total that adds up prices of cart items

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import './checkout.styles.scss';
 
-const Checkout = () => (
+const Checkout = ({ cartItems }) => (
 
   <div className='checkout'>
-    <div checkout-header>
-      <div checkout-header-title>Product</div>
-      <div checkout-header-title>Description</div>
-      <div checkout-header-title>Quantity</div>
-      <div checkout-header-title>Price</div>
-      <div checkout-header-title>Remove</div>
+    <div className='checkout-header'>
+      <div className='checkout-product checkout-header-title'>Product</div>
+      <div className='checkout-description checkout-header-title'>Description</div>
+      <div className='checkout-quantity checkout-header-title'>Quantity</div>
+      <div className='checkout-price checkout-header-title'>Price</div>
+      <div className='checkout-remove checkout-header-title'>Remove</div>
+    </div>
+    <div className='checkout-items'>
+      checkout items
+    </div>
+    <div className='total'>
+      TOTAL: ${ cartItems.reduce((acc, item) => {
+        return acc + item.quantity * item.price
+      }, 0) }
     </div>
   </div>
 
 )
 
-export default Checkout;
+const mapStateToProps = (state) => ({
+  cartItems: state.cart.cartItems
+})
+
+export default connect(mapStateToProps, null)(Checkout);
